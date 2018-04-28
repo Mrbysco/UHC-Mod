5a29baa4524ed22b3c8477293c55d5062d750cce
Mrbysco <Heeybysco@live.nl>
2018-04-28 22:37:24 +0200

    Fix nullpointer

--- src/main/java/com/Mrbysco/UHC/commands/CommandTreeUHC.java
+++ src/main/java/com/Mrbysco/UHC/commands/CommandTreeUHC.java
@@ -3,7 +3,6 @@ package com.Mrbysco.UHC.commands;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.server.MinecraftServer;
 import net.minecraftforge.server.command.CommandTreeBase;
 
@@ -40,9 +39,7 @@ public class CommandTreeUHC extends CommandTreeBase
 		{
 			player = (EntityPlayer) senderEntity;
 		}
-		final NBTTagCompound entityData = player.getEntityData();
-		
-        return server.isSinglePlayer() || super.checkPermission(server, sender) || (player != null && entityData.getBoolean("canEditUHC") == true);
+        return server.isSinglePlayer() || super.checkPermission(server, sender) || (player != null && player.getEntityData().getBoolean("canEditUHC") == true);
     }
 
     @Override
